ebd7701947b9b9c51a4f79636821f4dbc3e60a7b
ANSELMO HENRIQUE ASSUNÇÃO REIS <anselmo_reis@estudante.sesisenai.org.br>
2023-06-29 11:05:56 -0300

    alteração

---
+++ Drawer/src/Components/TelaA.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View } from 'react-native';
+
+
+export function TelaA() {
+  return (
+    <View style={styles.container}>
+
+    </View>
+  );
+}
---
+++ Drawer/src/Components/TelaB.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View } from 'react-native';
+
+export function TelaB() {
+  return (
+    <View style={styles.container}>
+
+    </View>
+  );
+}
